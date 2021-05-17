a6626d9755375557128879177046f158aaf9f93c
Hamed Moghadasi <h4lmed@gmail.com>
2021-05-17 14:50:56 +0430

    Correct return values

--- VersionUp.Core/Program.cs
+++ VersionUp.Core/Program.cs
@@ -18,7 +18,7 @@ namespace VersionUp.Core
                 if (args.Length == 0)
                 {
                     ConsoleHelper.ShowUsageHelp();
-                    return -1;
+                    return 1;
                 }
                 else
                 {
@@ -27,13 +27,13 @@ namespace VersionUp.Core
 
                 UpdateVersionNode(projectPath);
 
-                return 1;
+                return 0;
 
             }
             catch (Exception e)
             {
                 ConsoleHelper.WriteLine(e.StackTrace, ConsoleColor.Red);
-                throw;
+                return 1;
             }
         }
 
@@ -50,20 +50,23 @@ namespace VersionUp.Core
                 var version = new Version(versionNode);
                 version.Renew();
                 versionNode.InnerText = version.ToString();
-                xmldoc.Save(projectPath);
 
                 ConsoleHelper.WriteLine($"Application version updated to {versionNode.InnerText}", ConsoleColor.Green);
             }
             else
             {
                 //Create Base Version
-                var verNode = xmldoc.CreateElement("Version");
-                verNode.InnerText = "1.0.0.0";
-                root.AppendChild(verNode);
+
+                var propertyGroup = root.SelectSingleNode("PropertyGroup");
+                XmlNode verNode = xmldoc.CreateElement("Version");
+                verNode.InnerText = $"1.0.0.0";
+                propertyGroup.AppendChild(verNode);
 
                 ConsoleHelper.WriteLine($"Application version created {verNode.InnerText}", ConsoleColor.Green);
             }
-            
+
+            xmldoc.Save(projectPath);
+
         }
     }
 }
